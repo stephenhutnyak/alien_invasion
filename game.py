e819b3795fd981ac0c181d5967f37feb139324fb
stephenhutnyak <m252724@usna.edu>
2022-10-18 09:18:31 -0400

    Update game.py to create the ship and call the blitme() method

--- game.py
+++ game.py
@@ -2,6 +2,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -15,6 +16,8 @@ class AlienInvasion:
         )
         pygame.display.set_caption("Alien Invation")
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """Start the main loop for the fame."""
         while True:
@@ -25,6 +28,7 @@ class AlienInvasion:
 
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
