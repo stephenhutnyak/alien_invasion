b081a7d8a7c5d6fa5cdab108cce8b5d6092ca270
stephenhutnyak <m252724@usna.edu>
2022-10-18 09:21:08 -0400

    Update settings.py to correct for screen size

--- settings.py
+++ settings.py
@@ -7,5 +7,5 @@ class Settings:
         """Initialize the game's settings."""
         # Screen settings
         self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_height = 600
         self.bg_color = (0,0,153)
