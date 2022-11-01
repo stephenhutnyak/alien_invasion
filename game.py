9579d20a0bd40673c31183ee4817278dd4fbce3a
stephenhutnyak <m252724@usna.edu>
2022-11-01 08:43:25 -0400

    Add move right and left

--- game.py
+++ game.py
@@ -30,6 +30,13 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right
+                    self.ship.rect.x += 1
+                elif event.key == pygame.K_LEFT:
+                    # Move ship to the left
+                    self.ship.rect.x += 1
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
