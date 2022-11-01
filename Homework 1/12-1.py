50b6c865e49842789c20576cb202ff759474adb3
stephenhutnyak <m252724@usna.edu>
2022-11-01 08:11:21 -0400

    Added two new ship.png images

---
+++ Homework 1/12-1.py
@@ -0,0 +1,24 @@
+import sys
+
+import pygame
+
+class GameTime:
+
+    def __init__(self):
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Game Time")
+
+    def run_game(self):
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT():
+                    sys.exit()
+
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    ai = GameTime()
+    ai.run_game()
