4d56486bf4924e02fb5682707887e8bb81b59344
stephenhutnyak <m252724@usna.edu>
2022-11-01 09:36:25 -0400

    Add Bullet settings

--- settings.py
+++ settings.py
@@ -8,7 +8,14 @@ class Settings:
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 600
-        self.bg_color = (50,50,50)
+        self.bg_color = (50, 50, 50)
 
         # Ship settings
         self.ship_speed = 1.5
+
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (252, 3, 11)
+        
