c9f244611e1f76cf56dc9af8244f7abde6aaccef
stephenhutnyak <m252724@usna.edu>
2022-11-06 22:56:03 -0500

    Homework 1 Submission
    Not completed

--- Homework 1/settings.py
+++ Homework 1/settings.py
@@ -7,16 +7,18 @@ class GameTime:
     def __init__(self):
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode((1200, 600))
         pygame.display.set_caption("Game Time")
+        self.bg_color = (0, 255, 255)
 
     def run_game(self):
 
         while True:
             for event in pygame.event.get():
-                if event.type == pygame.QUIT():
+                if event.type == pygame.QUIT:
                     sys.exit()
 
+            self.screen.fill(self.bg_color)
             pygame.display.flip()
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -18,4 +18,3 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (252, 3, 11)
-        
