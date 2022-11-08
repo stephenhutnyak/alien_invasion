eec19f6d737dd2c7d9c2bf22be1ba4a1b326ab70
stephenhutnyak <m252724@usna.edu>
2022-11-08 09:25:30 -0500

    Create top row of aliens

--- game.py
+++ game.py
@@ -24,11 +24,6 @@ class AlienInvasion:
 
         self._create_fleet()
 
-    def _create_fleet(self):
-        """Create the fleet of aliens"""
-        #Make an alien
-        alien = Alien(self)
-        self.aliens.add(alien)
     def run_game(self):
         """Start the main loop for the game."""
         while True:
@@ -37,6 +32,22 @@ class AlienInvasion:
             self._update_bullets()
             self._update_screen()
 
+    def _create_fleet(self):
+        """Create the fleet of aliens"""
+        #Create an alien and find the number of aliens in a row
+        #Spacing between each alien is equal to one alien width
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        #Create the first row of aliens
+        for alien_number in range(number_aliens_x):
+            #Create an alien and place it in the row
+            alien = Alien(self)
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
 
     # Watch for keyboard and mouse events
     def _check_events(self):
