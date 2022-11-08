2036c83e02f03d9f4854a695bb5311d74a6e5118
stephenhutnyak <m252724@usna.edu>
2022-11-08 08:35:01 -0500

    Reorganize bullets update into method

--- game.py
+++ game.py
@@ -25,13 +25,9 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
 
-            #Get rid of the bullets that have disappeared
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
 
     # Watch for keyboard and mouse events
     def _check_events(self):
@@ -69,6 +65,16 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """update position of bullets and get rid of old bullets"""
+        #Update bullet position
+        self.bullets.update()
+
+        # Get rid of the bullets that have disappeared
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
