4e11d60c5b997a950e88b062148b96e476e25677
stephenhutnyak <m252724@usna.edu>
2022-11-09 08:35:34 -0500

    Move the fleet down and change direction
    Issue with "check_edges" method remains unresolved.  Game still works

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 # This is our settings module
 
 class Settings:
-    """A classs to store all settings for Alien Invasion"""
+    """A class to store all settings for Alien Invasion"""
 
     def __init__(self):
         """Initialize the game's settings."""
@@ -19,3 +19,9 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (252, 3, 11)
         self.bullets_allowed = 3
+
+        # Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represents left
+        self.fleet_direction = 1
